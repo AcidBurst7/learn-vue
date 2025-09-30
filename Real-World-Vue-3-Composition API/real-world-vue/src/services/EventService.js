7fc5314346aceda1f865392a9a3706d8087d3783
varak <varaksinaalla@mail.ru>
2025-09-30 13:57:13 +0300

    add json-server, add service - events list

--- Real-World-Vue-3-Composition API/real-world-vue/src/services/EventService.js
+++ Real-World-Vue-3-Composition API/real-world-vue/src/services/EventService.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+    baseURL: 'http://localhost:3000',
     withCredentials: false,
-    header: {
+    headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     }
